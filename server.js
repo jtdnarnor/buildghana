1569552fd8c631805cee47a99f94a468ce4efbb7
jtdnarnor <jtdnarnor@gmail.com>
2025-04-02 14:41:05 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+// Initialize Express
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/',
+  createParentPath: true,
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+}));
+app.use(cors());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/projects', require('./routes/api/projects'));
+app.use('/api/sectors', require('./routes/api/sectors'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/upload', require('./routes/api/upload'));
+
+// Serve uploaded files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
